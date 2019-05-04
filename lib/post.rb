09565a49ba7711f98c6a2b36e7eb88a3a9611614
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-04 07:53:47 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,17 @@
+class Author
+  attr_accessor :name 
+  
+  def initialize
+    @name = name
+end
+end
+
+class Post 
+  attr_accessor :title, :author
+  
+  def initialize
+    @title = title
+    @author = author
+end
+end
+
